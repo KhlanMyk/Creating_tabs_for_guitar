61a2502dca950bf754b01fb476f9251acb94d578
Mykyta Khlan <nik2004kh@gmail.com>
2026-02-16 17:09:32 +0000

    Add main application structure

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("test")
+"""Main file"""
+from audio_processor import AudioProcessor
+from pitch_detector import PitchDetector
+from tab_generator import GuitarTabGenerator
+
+def main():
+    print("Guitar Tab Generator")
+    AudioProcessor()
+
+if __name__ == "__main__":
+    main()
